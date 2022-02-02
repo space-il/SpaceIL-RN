b9b9e530aeebce87f4b1e5d286c88afe297b9973
pa7745 <pesya.avramovich@gmail.com>
2022-02-02 15:20:10 +0200

    Add filter bubbles

--- src/constants/Colors.ts
+++ src/constants/Colors.ts
@@ -2,6 +2,10 @@ export const COLORS = {
   BG_DEFAULT_TEMP: '#dad',
   LIGHT_GREY: '#bbb',
   LIGHT_BLUE: '#ADD8E6',
+  BLUE: '#0274BB',
   LIGHT_GREEN: '#90ee90',
   LIGHT_ORANGE: '#fed8b1',
+  WHITE: '#FFFFFF',
+  GRAY: '#E3E5EC',
+  BLACK: '#141622',
 };
