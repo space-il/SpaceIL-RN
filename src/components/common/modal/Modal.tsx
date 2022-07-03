59b92960a13038886ade8a123a564a27399bfc24
Sandler, Alexey(as371x) <as371x@intl.att.com>
2022-07-03 14:30:30 +0300

    Make Modal.tsx be generic. Refactor ErrorModal.tsx

--- src/components/common/modal/Modal.tsx
+++ src/components/common/modal/Modal.tsx
@@ -1,89 +1,19 @@
-import React, { FC, useEffect, useState } from 'react';
-import { StyleSheet, TextStyle, View } from 'react-native';
-import RNModal, { ModalProps } from 'react-native-modal';
-import { BaseText } from '@components/common/BaseText';
-import { Button } from '@components/common/buttons/Button';
-import { COLORS } from '@constants/Colors';
+import React, { FC, ReactElement, useEffect, useState } from 'react';
+import RNModal from 'react-native-modal';
 
 interface Props {
   isVisible: boolean;
-  modalTitleText: string;
-  primaryBtnLabel: string;
-  modalDescriptionText?: string;
-  secondaryBtnLabel?: string;
-  customModalProps?: ModalProps;
-  primaryBtnPressHandler?: () => void;
-  secondaryBtnPressHandler?: () => void;
-  customTitleStyle?: TextStyle;
+  children: ReactElement;
 }
 
-export const Modal: FC<Props> = ({
-  isVisible = false,
-  customModalProps,
-  modalTitleText = 'אופס משהו קרה',
-  modalDescriptionText,
-  primaryBtnLabel = 'אישור',
-  secondaryBtnLabel,
-  primaryBtnPressHandler,
-  secondaryBtnPressHandler,
-  children,
-  customTitleStyle,
-}) => {
+export const Modal: FC<Props> = (props: Props) => {
+  const { isVisible, children } = props;
   const [isModalVisible, setIsModalVisible] = useState(isVisible);
-  const modalProps = { backdropOpacity: 0.5, ...customModalProps, isVisible: isModalVisible };
-
-  console.log('customModalProps', customModalProps);
+  const modalProps = { backdropOpacity: 0.5, isVisible: isModalVisible };
 
   useEffect(() => {
     setIsModalVisible(isVisible);
   }, [isVisible]);
 
-  const closeModal = () => setIsModalVisible(false);
-
-  return (
-    <RNModal {...modalProps}>
-      <View style={styles.modalContent}>
-        <BaseText text={modalTitleText} customTextStyle={customTitleStyle} />
-        {modalDescriptionText && <BaseText text={modalDescriptionText} customTextStyle={styles.modalDescription} />}
-        {children}
-        <Button
-          btnLabel={primaryBtnLabel}
-          onPress={primaryBtnPressHandler || closeModal}
-          customBtnContainerStyle={styles.primaryBtnContainer}
-        />
-        {secondaryBtnLabel && (
-          <Button
-            secondaryBtn
-            btnLabel={secondaryBtnLabel}
-            onPress={secondaryBtnPressHandler || closeModal}
-            customBtnContainerStyle={styles.secondaryBtnContainer}
-          />
-        )}
-      </View>
-    </RNModal>
-  );
+  return <RNModal {...modalProps}>{children}</RNModal>;
 };
-
-const styles = StyleSheet.create({
-  modalContent: {
-    backgroundColor: COLORS.WHITE,
-    padding: 25,
-    borderRadius: 12,
-  },
-  modalTitle: {
-    fontSize: 20,
-    alignSelf: 'center',
-    marginBottom: 10,
-  },
-  primaryBtnContainer: {
-    marginTop: 10,
-  },
-  modalDescription: {
-    textAlign: 'center',
-    marginBottom: 20,
-  },
-  secondaryBtnContainer: {
-    marginTop: 5,
-    marginBottom: -10,
-  },
-});
--- src/components/errorModal/ErrorModal.tsx
+++ src/components/errorModal/ErrorModal.tsx
@@ -1,28 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Modal } from '@components/common/modal/Modal';
 import { useAppSelector } from '@app/storeUtils';
 import { selectErrorInfo } from '@app/appSelectors';
 import { ERROR_LABELS } from '@components/errorModal/consts';
-import { StyleSheet } from 'react-native';
+import { Button } from '@components/common/buttons/Button';
+import { StyleSheet, View } from 'react-native';
+import { BaseText } from '@components/common/BaseText';
+import { COLORS } from '@constants/Colors';
 
 export const ErrorModal = () => {
   const { isVisible, title, description, btnHandler } = useAppSelector(selectErrorInfo);
+  const [isModalVisible, setIsModalVisible] = useState(true);
+
+  const closeModal = () => setIsModalVisible(false);
 
   return (
-    <Modal
-      isVisible={isVisible}
-      primaryBtnPressHandler={btnHandler}
-      modalTitleText={title || ERROR_LABELS.TITLE}
-      primaryBtnLabel={description || ERROR_LABELS.BTN_LABEL}
-      customTitleStyle={styles.modalTitle}
-    />
+      <Modal isVisible={isModalVisible}>
+        <View style={styles.modalContent}>
+          <BaseText text={title || ERROR_LABELS.TITLE} customTextStyle={styles.modalTitle} />
+          {description && <BaseText text={description} customTextStyle={styles.modalDescription} />}
+          <Button
+              btnLabel={description || ERROR_LABELS.BTN_LABEL}
+              onPress={btnHandler || closeModal}
+              customBtnContainerStyle={styles.primaryBtnContainer}
+          />
+        </View>
+      </Modal>
   );
 };
 
 const styles = StyleSheet.create({
+  modalContent: {
+    backgroundColor: COLORS.WHITE,
+    padding: 25,
+    borderRadius: 12,
+  },
   modalTitle: {
     fontSize: 20,
     alignSelf: 'center',
     marginBottom: 10,
+  },
+  primaryBtnContainer: {
+    marginTop: 10,
+  },
+  modalDescription: {
+    textAlign: 'center',
+    marginBottom: 20,
   },
 });
